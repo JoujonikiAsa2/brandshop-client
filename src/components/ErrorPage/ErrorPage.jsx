5883b6d92375d0e3c0ac4db2a7452ba66d3f45b6
joujonikiasa2 <joujonikiasaroy@gmail.com>
2023-10-18 15:45:01 +0600

    Updated the color of the home page button on the error page

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import errorImage from '../../assets/photos/Error/Connection Error.gif'
+import { FaArrowLeft } from "react-icons/fa";
 const ErrorPage = () => {
     return (
         <div className="h-[100vh] flex flex-col justify-center items-center space-y-8">
             <h2 className="text-3xl text-red-400 font-bold">Something went wrong! Please go back to the Home Page</h2>
-            <Link to='/'><button className="btn btn-sm bg-red-300 border-black hover:bg-transparent">Home Page</button></Link>
+            <Link to='/'><button className="btn btn-lg text-[#FFF] text-lg capitalize bg-[#3876BF] border-black hover:bg-transparent hover:text-[#3876BF]"><FaArrowLeft></FaArrowLeft>Home Page</button></Link>
             <div>
                 <img src={errorImage} alt="" className="h-96"/>
             </div>
